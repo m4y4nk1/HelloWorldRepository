8cea08a11f2dbe89cb36baed57f890a1c57260cd
Mayank Mahajan <135986552+m4y4nk1@users.noreply.github.com>
2026-02-06 10:54:34 +0530

    Add message indicating code execution from GitHub

--- hello-java/HelloWorld.java
+++ hello-java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
   public static void main(String[]args){
     System.out.println("Hello, Jenkins");
+    System.out.println("Mayank Executed the java code from github");
   }
 }
